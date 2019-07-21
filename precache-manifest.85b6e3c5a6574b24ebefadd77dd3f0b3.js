4d81dbb902212d129dc2aca7b76751430f840b84
Your Name <nickliliakides@gmail.com>
2019-07-21 01:03:11 +0100

    Updates

---
+++ precache-manifest.85b6e3c5a6574b24ebefadd77dd3f0b3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "726cb8200b49001b639302a5f4f9a1f6",
+    "url": "/TechConnect/index.html"
+  },
+  {
+    "revision": "6cd343b6274823f589b4",
+    "url": "/TechConnect/static/css/main.700d3d42.chunk.css"
+  },
+  {
+    "revision": "c0b4c97261ead3e5184d",
+    "url": "/TechConnect/static/js/2.9721f4ac.chunk.js"
+  },
+  {
+    "revision": "6cd343b6274823f589b4",
+    "url": "/TechConnect/static/js/main.0a7d6f1d.chunk.js"
+  },
+  {
+    "revision": "ea1ff2c79b56e5165ec3",
+    "url": "/TechConnect/static/js/runtime~main.4d86dff3.js"
+  },
+  {
+    "revision": "c222e8ebef8d6703295f4f2fbb01ee12",
+    "url": "/TechConnect/static/media/showcase.c222e8eb.jpg"
+  },
+  {
+    "revision": "bc81ee218c5d21a3bfa0a132cec23ff2",
+    "url": "/TechConnect/static/media/spinner.bc81ee21.gif"
+  }
+]);
